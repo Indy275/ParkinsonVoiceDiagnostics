1f4d88bd8e435685535a7d98db2c88a4f41b25ad
Indy <indydolmans@gmail.com>
2024-09-27 14:15:35 +0200

    Removed main usage at this point within the project

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import pandas as pd
 
-path = "C:\\Users\\INDYD\\Documents\\RAIVD_data\\NeuroVoz\\audios\\"
+import seaborn as sns
+
+store_location = 'C:\\Users\\INDYD\\Documents\\RAIVD_data\\preprocessed_data\\NeuroVoz_preprocessed\\'
 
 sr = 44100  # Sampling rate
 frame_size = 500  # Number of samples per frame
@@ -12,34 +14,4 @@ fmin = 20  # Min frequency to use in Mel spectrogram
 fmax = sr // 2  # Max frequency; Nyquist frequency
 n_mels = 28  # Number of mel bands to generate
 
-files = ['HC_A1_0034.wav', 'PD_A2_0040.wav']
-
-for file in files:
-    x, _ = librosa.core.load(path+file, sr=sr)
-    plt.figure(figsize=(10, 6))
-
-    plt.subplot(311)
-    plt.plot(x)
-    plt.title(file[:-4])
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.grid(True, which='major', axis='x')
-    plt.tight_layout()
-
-    plt.subplot(312)
-    spectrogram = librosa.feature.melspectrogram(x[:-1],
-                                                sr=sr,
-                                                n_mels=n_mels,
-                                                hop_length=frame_step,
-                                                n_fft=frame_size,
-                                                fmin=fmin,
-                                                fmax=fmax)
-    spectrogram = librosa.power_to_db(spectrogram).astype(np.float32)
-    librosa.display.specshow(spectrogram,
-                            x_axis='time',
-                            y_axis='mel',
-                            sr=sr,
-                            hop_length=frame_step,
-                            fmax=fmax)
-    plt.tight_layout()
-    plt.show()
+################## To be used later on in the project, to run various pipelines easily at once. Currently not used.
