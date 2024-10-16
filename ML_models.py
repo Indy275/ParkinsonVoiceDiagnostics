fec07c2bf06cf129d204e576c4744113b95e847f
Indy <indydolmans@gmail.com>
2024-10-16 16:26:22 +0200

    Code reorganization: TL and monolingual corresponding functions

--- ML_models.py
+++ ML_models.py
@@ -1,8 +1,13 @@
-from sklearn.ensemble import RandomForestClassifier
+import random
+import os
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+import configparser
 import matplotlib.pyplot as plt
 
+from sklearn.ensemble import RandomForestClassifier
 from eval import evaluate_predictions
-import configparser
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
@@ -35,8 +40,6 @@ def run_ml_model(X_train, X_test, y_train, y_test, df, test_indices):
         shimmer_features = [str(x) for x in range(fl[3], fl[4])]
         mfcc_features = [str(x) for x in range(fl[4], fl[5])]
 
-        sum_mfcc = sum(val for key, val in fimp if key in mfcc_features)
-        print("Contribution of MFCC:", sum_mfcc)
         sum_f0 = sum(val for key, val in fimp if key in f0_features)
         print("Contribution of F0:", sum_f0)
         sum_form = sum(val for key, val in fimp if key in formant_features)
@@ -45,6 +48,8 @@ def run_ml_model(X_train, X_test, y_train, y_test, df, test_indices):
         print("Contribution of Jitter:", sum_jit)
         sum_shim = sum(val for key, val in fimp if key in shimmer_features)
         print("Contribution of Shimmer:", sum_shim)
+        sum_mfcc = sum(val for key, val in fimp if key in mfcc_features)
+        print("Contribution of MFCC:", sum_mfcc)
         print("Total feature importance (should equal to 1):", sum_mfcc + sum_f0 + sum_form + sum_jit + sum_shim)
 
         # fimp_mfcc = [(key, val) for key, val in fimp if key in mfcc_features]
@@ -52,7 +57,12 @@ def run_ml_model(X_train, X_test, y_train, y_test, df, test_indices):
         plt.barh(df.columns[fl[1]:fl[2]], clf.feature_importances_[fl[1]:fl[2]], color='blue')
         plt.barh(df.columns[fl[2]:fl[3]], clf.feature_importances_[fl[2]:fl[3]], color='red')
         plt.barh(df.columns[fl[3]:fl[4]], clf.feature_importances_[fl[3]:fl[4]], color='purple')
-        plt.barh(df.columns[fl[4]:fl[5]], clf.feature_importances_[fl[4]:fl[5]], color='orange')
+        # plt.barh(df.columns[fl[4]:fl[5]], clf.feature_importances_[fl[4]:fl[5]], color='orange')
+
+        plt.barh(df.columns[fl[4]:fl[4]+39], clf.feature_importances_[fl[4]:fl[4]+39], color='green')
+        plt.barh(df.columns[fl[4]+39:fl[4]+39+39], clf.feature_importances_[fl[4]+39:fl[4]+39+39], color='blue')
+        plt.barh(df.columns[fl[4]+39+39:fl[4]+78+39], clf.feature_importances_[fl[4]+39+39:fl[4]+78+39], color='red')
+        plt.barh(df.columns[fl[4]+78+39:fl[4]+78+78], clf.feature_importances_[fl[4]+78+39:fl[4]+78+78], color='orange')
 
         plt.yticks(
             [(fl[0] + fl[1]) / 2, (fl[1] + fl[2]) / 2, (fl[2] + fl[3]) / 2, (fl[3] + fl[4]) / 2, (fl[4] + fl[5]) / 2],
@@ -65,3 +75,55 @@ def run_ml_model(X_train, X_test, y_train, y_test, df, test_indices):
     return file_scores, subj_scores
 
 
+def train_model(model, X, y):
+    model.fit(X, y)
+
+def run_ml_tl_model(base_X_train, base_X_test, base_y_train, base_y_test, tgt_df):
+    n_features = np.shape(base_X_train)[1]
+    clf = RandomForestClassifier()
+    train_model(clf, base_X_train, base_y_train)
+
+    pos_subjs = list(tgt_df[tgt_df['y'] == 1]['subject_id'].unique())
+    neg_subjs = list(tgt_df[tgt_df['y'] == 0]['subject_id'].unique())
+    max_shot = min(len(pos_subjs), len(neg_subjs)) - 5
+
+    metrics_list, metrics_grouped, n_tgt_train_samples, base_metrics = [], [], [], []
+    seed = int(random.random()*10000)
+    for n_shots in range(max_shot):
+        clf_copy = deepcopy(clf)  # Copy model trained on base language
+
+        random.seed(seed)
+        pos_train_samples = random.sample(pos_subjs, n_shots)
+        random.seed(seed)
+        neg_train_samples = random.sample(neg_subjs, n_shots)
+
+        tgt_train_df = tgt_df[tgt_df['subject_id'].isin(pos_train_samples + neg_train_samples)]
+        tgt_test_df = tgt_df[~tgt_df['subject_id'].isin(pos_train_samples + neg_train_samples)]
+
+        if n_shots > 0:
+            # Train model with these additional samples
+            tgt_X_train = tgt_train_df.iloc[:, :n_features]
+            tgt_y_train = tgt_train_df['y']
+            train_model(clf_copy, tgt_X_train, tgt_y_train)
+
+        tgt_X_test = tgt_test_df.iloc[:, :n_features]
+        tgt_y_test = tgt_test_df['y']
+
+        predictions = clf_copy.predict(tgt_X_test)
+        tgt_test_df.loc[:, 'preds'] = predictions
+
+        metrics_list.append(evaluate_predictions('RFC' + '0{}shot'.format(n_shots), tgt_y_test, predictions))
+        n_tgt_train_samples.append(n_shots)
+
+        base_metrics.append(evaluate_predictions('RFC' + ' BASEDF', base_y_test, clf_copy.predict(base_X_test)))
+
+        samples_preds = tgt_test_df.groupby('sample_id').agg({'preds': lambda x: x.mode()[0]}).reset_index()
+        samples_ytest = tgt_test_df.groupby('sample_id').agg({'y': lambda x: x.mode()[0]}).reset_index()
+
+        metrics_grouped.append(
+            evaluate_predictions('RFC' + 'Sample', samples_ytest['y'].tolist(),
+                                    samples_preds['preds'].tolist()))
+    
+    # print("Metrics:\n", metrics_list, "\n grouped: \n", metrics_grouped, "\n base \n", base_metrics, "\n", n_tgt_train_samples)
+
+    return metrics_list, metrics_grouped, base_metrics, n_tgt_train_samples
--- run_experiments.py
+++ run_experiments.py
@@ -1,13 +1,15 @@
-import numpy as np
-import configparser
-from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
-import pandas as pd
 import os
 from copy import deepcopy
+import numpy as np
+import pandas as pd
+import configparser
+
+from sklearn.model_selection import StratifiedKFold
 
 from data_util import load_data, scale_features
 from DNN_models import run_dnn_model
 from ML_models import run_ml_model
+from ML_models import run_ml_tl_model
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
@@ -25,7 +27,6 @@ def run_data_fold(model, df, n_features, train_indices, test_indices):
     X_test = df.loc[test_indices, df.columns[:n_features]]
     y_train = df.loc[train_indices, 'y']
     y_test = df.loc[test_indices, 'y']
-
     if print_intermediate:
         print("Train subjects:", np.sort(df.loc[train_indices, 'subject_id'].unique()))
         print("Test subjects:", np.sort(df.loc[test_indices, 'subject_id'].unique()))
@@ -43,33 +44,28 @@ def run_data_fold(model, df, n_features, train_indices, test_indices):
         return run_dnn_model(X_train, X_test, y_train, y_test, df, test_indices)
 
 
-def run(dataset, ifm_nifm, model, k=1):
+def run_monolingual(dataset, ifm_nifm, model, k=2):
     df, n_features = load_data(dataset, ifm_nifm)
     print("Data shape:", df.shape)
 
     fscores, sscores = [], []
     split_df = df.drop_duplicates(['subject_id'])
-    split_df.loc[:,'ygender'] = split_df['y'].astype(str) + '_'# + split_df['gender'].astype(str)
-    print(split_df['ygender'].value_counts())
+    split_df.loc[:,'ygender'] = split_df['y'].astype(str) + '_' + split_df['gender'].astype(str)
 
     kf = StratifiedKFold(n_splits=k, shuffle=True)
-    
     for i, (train_split_indices, test_split_indices) in enumerate(kf.split(split_df['subject_id'], split_df['ygender'])):
         print(f"Running {model} with data fold {i} of {k}")
-        df = deepcopy(df)
+        df_copy = deepcopy(df)
 
         # Crazy hack needed because split() function stupidity... only works if sample_id is unique
         trainsamples = split_df.iloc[train_split_indices]['sample_id']
         testsamples = split_df.iloc[test_split_indices]['sample_id']
-        train_indices = df[df['sample_id'].isin(trainsamples)].index.tolist()
-        test_indices = df[df['sample_id'].isin(testsamples)].index.tolist()
+        train_indices = df_copy[df_copy['sample_id'].isin(trainsamples)].index.tolist()
+        test_indices = df_copy[df_copy['sample_id'].isin(testsamples)].index.tolist()
 
-        print(train_split_indices, test_split_indices)
-        print(df.iloc[test_split_indices])
-    
-        df = scale_features(df, n_features, train_indices, test_indices)
+        df_copy = scale_features(df_copy, n_features, train_indices, test_indices)
 
-        fscore, sscore = run_data_fold(model, df, n_features, train_indices, test_indices)
+        fscore, sscore = run_data_fold(model, df_copy, n_features, train_indices, test_indices)
         fscores.append(fscore)
         sscores.append(sscore)
 
@@ -86,4 +82,66 @@ def run(dataset, ifm_nifm, model, k=1):
     print("Mean Spec:", round(sum(i[3] for i in sscores)/k, 3))
 
 
+def run_data_fold_tl(model, base_df, n_features, base_train_idc, base_test_idc, tgt_df):
+    base_X_train = base_df.loc[base_train_idc, base_df.columns[:n_features]]
+    base_X_test = base_df.loc[base_test_idc, base_df.columns[:n_features]]
+    base_y_train = base_df.loc[base_train_idc, 'y']
+    base_y_test = base_df.loc[base_test_idc, 'y']
+    if print_intermediate:
+        print("Train subjects:", np.sort(base_df.loc[base_train_idc, 'subject_id'].unique()))
+        print("Test subjects:", np.sort(base_df.loc[base_test_idc, 'subject_id'].unique()))
+        print("Train/test shapes:", base_X_train.shape, base_X_test.shape, base_y_train.shape, base_y_test.shape)
+    if model == 'RFC':
+        return run_ml_tl_model(base_X_train, base_X_test, base_y_train,  base_y_test, tgt_df)
+    elif model == 'DNN':
+        import tensorflow as tf
+        from DNN_models import run_dnn_model
+        base_X_train = tf.convert_to_tensor(base_X_train)
+        base_X_test = tf.convert_to_tensor(base_X_test)
+        base_y_train = tf.convert_to_tensor(base_y_train)
+        base_y_test = tf.convert_to_tensor(base_y_test)
 
+        return run_dnn_model(base_X_train, base_X_test, base_y_train, base_y_test, tgt_df)
+    
+
+def run_crosslingual(base_dataset, target_dataset, ifm_nifm, model, k=2):
+    base_df, base_features = load_data(base_dataset, ifm_nifm)
+    target_df, target_features = load_data(target_dataset, ifm_nifm)
+    assert base_features == target_features, "Number of features across languages should be equal: {} and {}".format(
+        base_features, target_features)
+    print("Data shapes:", base_df.shape, target_df.shape)
+
+    metrics_list, metrics_grouped, base_metrics = [], [], []
+    base_df_split = base_df.drop_duplicates(['subject_id'])
+    base_df_split.loc[:,'ygender'] = base_df_split['y'].astype(str) + '_' + base_df_split['gender'].astype(str)
+
+    kf = StratifiedKFold(n_splits=k, shuffle=True)
+    for i, (train_split_indices, test_split_indices) in enumerate(kf.split(base_df_split['subject_id'], base_df_split['ygender'])):
+        print(f"Running {model} with data fold {i} of {k}")
+        df_copy = deepcopy(base_df)
+
+        # Crazy hack needed because split() function stupidity... only works if sample_id is unique
+        trainsamples = base_df_split.iloc[train_split_indices]['sample_id']
+        testsamples = base_df_split.iloc[test_split_indices]['sample_id']
+        train_indices = df_copy[df_copy['sample_id'].isin(trainsamples)].index.tolist()
+        test_indices = df_copy[df_copy['sample_id'].isin(testsamples)].index.tolist()
+
+        df_copy = scale_features(df_copy, base_features, train_indices, test_indices)
+
+        metrics_li, metrics_grouped_li, metrics_base_li, n_tgt_train_samples = run_data_fold_tl(model, base_df, base_features, train_indices, test_indices, target_df)
+        metrics_list.append(metrics_li)
+        metrics_grouped.append(metrics_grouped_li)
+        base_metrics.append(metrics_base_li)
+
+    metrics_df = pd.DataFrame(np.mean(metrics_list, axis=0), columns=['Accuracy', 'ROC_AUC', 'Sensitivity', 'Specificity'])
+    metrics_df['Iteration'] = n_tgt_train_samples
+    metrics_df.to_csv(os.path.join('experiments', f'RFC_{ifm_nifm}_metrics_{base_dataset}_{target_dataset}.csv'), index=False)
+
+    base_metrics_df = pd.DataFrame(np.mean(base_metrics,axis=0), columns=['Accuracy', 'ROC_AUC', 'Sensitivity', 'Specificity'])
+    base_metrics_df['Iteration'] = n_tgt_train_samples
+    base_metrics_df.to_csv(os.path.join('experiments', f'RFC_{ifm_nifm}_metrics_{base_dataset}_{target_dataset}_base.csv'), index=False)
+    
+    metrics_grouped_df = pd.DataFrame(np.mean(metrics_grouped,axis=0), columns=['Accuracy', 'ROC_AUC', 'Sensitivity', 'Specificity'])
+    metrics_grouped_df['Iteration'] = n_tgt_train_samples
+    metrics_grouped_df.to_csv(os.path.join('experiments', f'RFC_{ifm_nifm}_metrics_{base_dataset}_{target_dataset}_grouped.csv'), index=False)
+    print(f'Metrics saved to: experiments/RFC_{ifm_nifm}_metrics_{base_dataset}_{target_dataset}_grouped.csv')
