88131c2fd40ec062315b423e6116d97d7c3aa11c
Indy <indydolmans@gmail.com>
2024-11-08 14:55:20 +0100

    Changed to include high-lowpass filter

--- loudness_normalization.sh
+++ loudness_normalization.sh
@@ -3,20 +3,25 @@
 # in_dir: directory containing recordings to be normalized
 # out_dir: output directory containing recordings normalized.
 
-# NeuroVoz tdu
-# Neurovoz ddk
-# Italian tdu
+# NeuroVoz tdu done
+# Neurovoz ddk done
+# Italian tdu done
 # Italian ddk
 # PCGITA tdu
 # PCGITA ddk
 
-in_dir=C:\\Users\\INDYD\\Documents\\RAIVD_data\\NeuroVoz\\records_tdu\\
-out_dir=C:\\Users\\INDYD\\Documents\\RAIVD_data\\NeuroVoz\\records_tdu_norm\\
+in_dir=C:\\Users\\INDYD\\Documents\\RAIVD_data\\ItalianPD\\records_ddk\\
+temp_dir=C:\\Users\\INDYD\\Documents\\RAIVD_data\\NeuroVoz\\records_temp\\
+out_dir=C:\\Users\\INDYD\\Documents\\RAIVD_data\\ItalianPD\\records_ddk_norm\\
 
 for audio_File in "$in_dir"/*
 do
 	uttID=$(basename "$audio_File")
+  temp_path=$temp_dir$uttID
+  echo $temp_path
+	ffmpeg-normalize $audio_File -o $temp_path -f -ar 16000
   out_path=$out_dir$uttID
   echo $out_path
-	ffmpeg-normalize $audio_File -o $out_path -f -ar 16000
+  ffmpeg -i $temp_path -af "highpass=f=100, lowpass=f=3000" $out_path -loglevel error
+ 
 done
